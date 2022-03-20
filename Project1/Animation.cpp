092d471c6598b0919fcf2bfce2ca769a92dbf97b
Ashad001 <93534298+Ashad001@users.noreply.github.com>
2022-03-20 19:40:32 +0500

    Add files via upload

---
+++ Project1/Animation.cpp
@@ -0,0 +1,30 @@
+#include <SFML/Graphics.hpp>
+#include "Animation.h"
+
+Animation::Animation(sf::Texture* i_Tex, sf::Vector2u ImageCnt, float SwitchTm)
+{
+	this->imageCount = ImageCnt;
+	this->switchTime = SwitchTm;
+	TotalTime = 0.0f;
+	currentImage.x = 0;
+
+	uvRect.width = i_Tex->getSize().x / float(imageCount.x);
+	uvRect.height = i_Tex->getSize().y / float(imageCount.y);
+}
+
+void Animation::Update(int row, float Deltatime)
+{
+	currentImage.y = row;
+	TotalTime += Deltatime;
+	if (TotalTime >= switchTime)
+	{
+		TotalTime -= switchTime;
+		currentImage.x++;
+		if (currentImage.x >= imageCount.x)
+		{
+			currentImage.x = 0;
+		}
+	}
+	uvRect.left = currentImage.x * uvRect.width;
+	uvRect.top = currentImage.y * uvRect.height;
+}
---
+++ Project1/Animation.h
@@ -0,0 +1,14 @@
+#pragma once
+class Animation
+{
+private:
+	sf::Vector2u imageCount;
+	sf::Vector2u currentImage;
+	float TotalTime;
+	float switchTime;
+public:
+	sf::IntRect uvRect;
+	Animation(sf::Texture* i_Tex, sf::Vector2u ImageCnt, float SwitchTm);
+	void Update(int row, float Deltatime);
+};
+
