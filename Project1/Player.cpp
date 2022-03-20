40b2ed32be07bb576601be4dc64bd44dc57f711e
Ashad001 <93534298+Ashad001@users.noreply.github.com>
2022-03-20 19:43:24 +0500

    Update Player.cpp

--- Project1/Player.cpp
+++ Project1/Player.cpp
@@ -13,8 +13,7 @@ Player::Player(sf::RenderWindow& i_window, sf::Texture* tex, int MaxHp = 10)
 	HPBar.setFillColor(sf::Color::Blue);
 	HPBar.setSize(sf::Vector2f(HP * 5.0f, 7.0f));
 	HPBar.setPosition(player.getPosition().x + 50, player.getPosition().y + 90);
-	player.setPosition(-50.0f, i_window.getSize().y - player_texture->getSize().y / 2 - 100);
-	
+	player.setPosition(i_window.getSize().x / 3, i_window.getSize().y - player_texture->getSize().y / 2 - 100);
 }
 
 sf::Vector2u Player::GetPlayerSize()
@@ -37,24 +36,51 @@ sf::Vector2f Player::GetPlayerPosition()
 	return player.getPosition();
 }
 
-void Player::MovePLayer(sf::RenderWindow &i_window)
+void Player::MovePLayer(sf::RenderWindow &i_window , float deltaTime)
 {
-	SetPlayerPosition(sf::Mouse::getPosition(i_window).x, player.getPosition().y);
-	if (player.getPosition().x + 40 < 0)
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
 	{
-		SetPlayerPosition(-50.0f, i_window.getSize().y - player_texture->getSize().y / 2 - 100);
+		player.move(0.0f, -0.4f * deltaTime);
 	}
-	if (player.getPosition().x + player_texture->getSize().x + 350 >= i_window.getSize().x)
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
 	{
-		SetPlayerPosition(i_window.getSize().x - player_texture->getSize().x - 350, i_window.getSize().y - player_texture->getSize().y / 2 - 100);
+		player.move(0.0f, 0.4f * deltaTime);
 	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	{
+		player.move(- 0.4f * deltaTime, 0.0f);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	{
+		player.move(0.4f * deltaTime, 0.0f);
+	}
+	if (player.getPosition().x < 0)
+	{
+		player.setPosition(0.0f, player.getPosition().y);
+	}
+	if (player.getPosition().x + 470 > i_window.getSize().x)
+	{
+		player.setPosition(i_window.getSize().x - 470, player.getPosition().y );
+	}
+	if (player.getPosition().y - 250 < 0)
+	{
+		player.setPosition(player.getPosition().x , 250);
+	}
+	if (player.getPosition().y + 150 > i_window.getSize().y)
+	{
+		player.setPosition(player.getPosition().x,i_window.getSize().y - 150);
+	}
+	
+
+
 	SetHpBar(i_window);
+	SetHpBarSize();
 }
 
 
 void Player::SetHpBar(sf::RenderWindow &i_window)
 {
-	HPBar.setPosition(sf::Mouse::getPosition(i_window).x + 35, player.getPosition().y + 80);
+	HPBar.setPosition(player.getPosition().x  + player.getGlobalBounds().width / 4.0f, player.getPosition().y + player.getGlobalBounds().height / 0.9f);
 	if (HPBar.getPosition().x < 0)
 	{
 		HPBar.setPosition(0.0f, HPBar.getPosition().y);
