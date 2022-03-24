ca74a3f1c30b98e9b18cc487cf6aa118a307e4c0
Ashad001 <93534298+Ashad001@users.noreply.github.com>
2022-03-24 09:20:10 +0500

    Update Source.cpp

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,13 +1,13 @@
 #include <SFML/Graphics.hpp>
-#include<vector>
+#include <SFML/Audio.hpp>
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include<vector>
 #include "Bullet.h"
 #include "Player.h"
-#include <cstdlib>
 #include "Animation.h"
-#include <SFML/Audio.hpp>
 using namespace std;
-
 
 
 int main()
@@ -15,13 +15,46 @@ int main()
 	int Is_Game_Start = 0;
 	int startGame = 0;
 	int Is_Game_Pause = 0;
+	int HighScorePanel = 0;
+	int HighScoresOfPlayers[10] = { 0 }, countOfScores = 10;
+
+	// Reading Data of scores from file.
+	fstream HighScoresFile;
+	HighScoresFile.open("High Scores.txt", ios::in);
+	if (!HighScoresFile)
+	{
+		cout << "Unable to open the High Scores file" << endl;
+	}
+	int Temp_Score;
+	for (int i = 0; HighScoresFile >> Temp_Score; i++)
+	{
+		HighScoresOfPlayers[i] = Temp_Score;
+	}
+	HighScoresFile.close();
+	// Arranging in Desending order
+	for (int i = 0; i < countOfScores; i++)
+	{
+		for (int j = i; j < countOfScores; j++)
+		{
+			if (HighScoresOfPlayers[j] > HighScoresOfPlayers[i])
+			{
+				HighScoresOfPlayers[j] += HighScoresOfPlayers[i];
+				HighScoresOfPlayers[i] = HighScoresOfPlayers[j] - HighScoresOfPlayers[i];
+				HighScoresOfPlayers[j] = HighScoresOfPlayers[j] - HighScoresOfPlayers[i];
+			}
+		}
+	}
+
 	//Window
 	srand(time(NULL));
-	//sf::RenderWindow window(sf::VideoMode(700, 700), "Game");
+	// sf::RenderWindow window(sf::VideoMode(700, 700), "Game");
 	sf::RenderWindow window(sf::VideoMode(1380, 780), "Game", sf::Style::Fullscreen);
 	window.setFramerateLimit(60);
 
 	// DINEHS WORK 2
+	sf::RectangleShape HighScoresScreen(sf::Vector2f(window.getSize()));
+	HighScoresScreen.setFillColor(sf::Color::Black);
+
 	sf::RectangleShape MainScreen(sf::Vector2f(window.getSize()));
 	MainScreen.setFillColor(sf::Color(13, 13, 13));
 
@@ -36,6 +69,7 @@ int main()
 	sf::Text GameTitle;
 	sf::Font TitleFont;
 	TitleFont.loadFromFile("Bungee-Regular.ttf");
+
 	GameTitle.setFont(TitleFont);
 	GameTitle.setString("Space War");
 	GameTitle.setCharacterSize(MainScreen.getSize().y / 6.0);
@@ -48,12 +82,12 @@ int main()
 	Start.setCharacterSize(MainScreen.getSize().y / 20.0);
 	Start.setOrigin(Start.getGlobalBounds().width / 2.f, (Start.getGlobalBounds().height / 2.f) - GameTitle.getGlobalBounds().height);
 	Start.setStyle(sf::Text::Bold);
+	Start.setOutlineThickness(3);
 
 	sf::Text Pause;
 	Pause.setFont(TitleFont);
 	Pause.setString("PAUSE[P]");
 	Pause.setCharacterSize(MainScreen.getSize().y / 25.0);
-	//Pause.setOrigin(Pause.getGlobalBounds().width / 2.f, (Pause.getGlobalBounds().height / 2.f) - GameTitle.getGlobalBounds().height);
 	Pause.setPosition(window.getSize().x - 210, 500);
 	Pause.setStyle(sf::Text::Bold);
 	Pause.setOutlineThickness(3);
@@ -65,6 +99,8 @@ int main()
 	Quit.setCharacterSize(MainScreen.getSize().y / 20.0);
 	Quit.setOrigin(Quit.getGlobalBounds().width / 2.f, Quit.getGlobalBounds().height / 2.f);
 	Quit.setStyle(sf::Text::Bold);
+	Quit.setOutlineThickness(3);
+
 
 	sf::Text Restart;
 	Restart.setFont(TitleFont);
@@ -72,10 +108,17 @@ int main()
 	Restart.setCharacterSize(MainScreen.getSize().y / 20.0);
 	Restart.setOrigin(Restart.getGlobalBounds().width / 2.f, Restart.getGlobalBounds().height / 2.f);
 	Restart.setStyle(sf::Text::Bold);
+	Restart.setOutlineThickness(3);
 
+	sf::Text HighScores;
+	HighScores.setFont(TitleFont);
+	HighScores.setString("HIGH SCORES");
+	HighScores.setCharacterSize(MainScreen.getSize().y / 20.0);
+	HighScores.setOrigin(HighScores.getGlobalBounds().width / 2.f, HighScores.getGlobalBounds().height / 2.f);
+	HighScores.setStyle(sf::Text::Bold);
+	HighScores.setOutlineThickness(3);
 
-
-	sf::SoundBuffer buffer;	
+	sf::SoundBuffer buffer;
 	buffer.loadFromFile("shoot.wav");
 	sf::Sound Shoot;
 	Shoot.setBuffer(buffer);
@@ -86,8 +129,12 @@ int main()
 	sf::Sound Select;
 	Select.setBuffer(buffer_Select);
 	Select.setVolume(20.f);
-
-
+	
+	sf::Music Intro;
+	Intro.openFromFile("Intro.ogg");
+	Intro.setVolume(20.f);
+	Intro.play();
+	Intro.setLoop(1);
 
 	// DINESH WORK
 	sf::Texture Star01Pic;
@@ -179,7 +226,7 @@ int main()
 	sf::Sprite Heart;
 	Heart.setTexture(HeartTexture);
 	vector< sf::Sprite> Hearts;
-	Animation animaion_hearts(&HeartTexture, sf::Vector2u(4,1),100);
+	Animation animaion_hearts(&HeartTexture, sf::Vector2u(4, 1), 100);
 
 	sf::Texture AmnoTexture;
 	AmnoTexture.loadFromFile("Amno.png");
@@ -187,26 +234,26 @@ int main()
 	Amno.setTexture(AmnoTexture);
 	Amno.setScale(2.f, 2.f);
 	vector<sf::Sprite> Amnos;
-	Animation animation_amno(&AmnoTexture, sf::Vector2u(3,1), 100);
+	Animation animation_amno(&AmnoTexture, sf::Vector2u(3, 1), 100);
 
 	// GameOver
 	sf::Font font;
 	font.loadFromFile("Font.ttf");
-	sf::Text txt;
-	txt.setFont(font);
-	txt.setString("Game Over");
-	txt.setCharacterSize(30);
-	txt.setPosition(window.getSize().x / 2 - 80, window.getSize().y / 2 - 150);
-	txt.setFillColor(sf::Color::White);
-	txt.setOutlineThickness(2);
-	txt.setOutlineColor(sf::Color::Red);
+	sf::Text GameOver;
+	GameOver.setFont(font);
+	GameOver.setString("Game Over");
+	GameOver.setCharacterSize(30);
+	GameOver.setPosition(window.getSize().x / 2 - 80, window.getSize().y / 2 - 150);
+	GameOver.setFillColor(sf::Color::White);
+	GameOver.setOutlineThickness(2);
+	GameOver.setOutlineColor(sf::Color::Red);
 
 	//Score
 	sf::Text Score;
 	Score.setFont(TitleFont);
 	Score.setString("SCORE");
 	Score.setCharacterSize(MainScreen.getSize().y / 25.0);
-	Score.setPosition(window.getSize().x - 230, 200);
+	Score.setPosition(window.getSize().x - 245, 200);
 	Score.setFillColor(sf::Color::White);
 	Score.setOutlineThickness(3);
 	Score.setOutlineColor(sf::Color::Blue);
@@ -217,7 +264,7 @@ int main()
 	Count.setFont(TitleFont);
 	Count.setString(to_string(killCount));
 	Count.setCharacterSize(MainScreen.getSize().y / 25.0);
-	Count.setPosition(window.getSize().x - 180, 250);
+	Count.setPosition(window.getSize().x - 205.f, 250);
 	Count.setFillColor(sf::Color::White);
 	Count.setOutlineThickness(1);
 	Count.setOutlineColor(sf::Color::Blue);
@@ -231,19 +278,30 @@ int main()
 	string Power = "1212121212333451";
 	int powerCounter = 0;
 	int kill;
-	int dead1= -1, dead2 = -1;
+	int dead1 = -1, dead2 = -1;
 	int StarCount = 0;
 	int Heart_Move = 0;
 	int bullet_strength = 1;
+	int is_dead[500];
+	int pos = 0;
+	for (int i = 0; i < 500; i++)
+	{
+		is_dead[i] = 0;
+	}
 
 	while (window.isOpen())
 	{
 		Pause.setFillColor(sf::Color::White);
 		Pause.setOutlineColor(sf::Color::Blue);
 		Restart.setFillColor(sf::Color::White);
+		Restart.setOutlineColor(sf::Color::Blue);
 		GameTitle.setFillColor(sf::Color::White);
 		Start.setFillColor(sf::Color::White);
+		Start.setOutlineColor(sf::Color::Blue);
+		HighScores.setFillColor(sf::Color::White);
+		HighScores.setOutlineColor(sf::Color::Blue);
 		Quit.setFillColor(sf::Color::White);
+		Quit.setOutlineColor(sf::Color::Blue);
 
 		// Pause Game
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::P))
@@ -295,7 +353,6 @@ int main()
 			if (evnt.type == sf::Event::Resized)
 			{
 			}
-			
 		}
 
 		if (Is_Game_Pause == 0)
@@ -305,15 +362,15 @@ int main()
 			{
 				shootCounter++;
 			}
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && shootCounter >= 12)
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && shootCounter >= 12 && Is_Game_Start == 1)
 			{
 				playerShoot.SetBulletPosition(player.GetPlayerCentre().x - 56.0f, player.GetPlayerCentre().y - 20.0f);
 				projectiles.push_back(Bullet(playerShoot));
 				playerShoot.SetBulletPosition(player.GetPlayerCentre().x + 34.0f, player.GetPlayerCentre().y - 20.0f);
 				projectiles.push_back(Bullet(playerShoot));
-				
+
 				Shoot.play();
-				
+
 				shootCounter = 0;
 			}
 			for (int i = 0; i < projectiles.size(); i++)
@@ -339,11 +396,11 @@ int main()
 
 
 			// Enemies
-			if (respawn < 70)
+			if (respawn < 90 - killCount / 20.f)
 			{
 				respawn++;
 			}
-			if (respawn >= 70)
+			if (respawn + killCount / 20.f >= 90)
 			{
 				respawn = 0;
 				if (Power[powerCounter] == '1')
@@ -358,11 +415,11 @@ int main()
 				{
 					enemy.setColor(sf::Color::Red);
 				}
-				else if (Power[powerCounter] == '4')
+				else if (Power[powerCounter] == '4' && killCount >= 20 * player.GetHp())
 				{
 					enemy.setColor(sf::Color::Cyan);
 				}
-				else if (Power[powerCounter] == '5')
+				else if (Power[powerCounter] == '5' && killCount >= 70 * bullet_strength)
 				{
 					enemy.setColor(sf::Color::Magenta);
 				}
@@ -387,22 +444,21 @@ int main()
 			}
 			for (int i = 0; i < enemies.size() && Is_Game_Start == 1; i++)
 			{
-			
 				if (enemies[i].getColor() == sf::Color::Yellow)
 				{
-					enemies[i].move(0.0f, 0.8f + float(killCount / 150.0f));
+					enemies[i].move(0.0f, (0.7f + float(killCount / 150.0f)));
 				}
 				else if (enemies[i].getColor() == sf::Color::Green)
 				{
-					enemies[i].move(0.0f, 0.5f + float(killCount / 250.0f));
+					enemies[i].move(0.0f, (0.5f + float(killCount / 250.0f)));
 				}
 				else if (enemies[i].getColor() == sf::Color::Red)
 				{
-					enemies[i].move(0.0f, 0.3f + float(killCount / 350.0f));
+					enemies[i].move(0.0f, (0.3 + float(killCount / 350.0f)));
 				}
 				else if (enemies[i].getColor() == sf::Color::Cyan)
 				{
-					enemies[i].move(0.0f, 0.9f + float(killCount / 150.0f));
+					enemies[i].move(0.0f, (0.9 + float(killCount / 150.0f)));
 				}
 				else if (enemies[i].getColor() == sf::Color::Magenta)
 				{
@@ -418,6 +474,7 @@ int main()
 					player.SetHP(0);
 				}
 			}
+
 			// collision
 			for (int i = 0; i < enemies.size(); i++)
 			{
@@ -437,7 +494,7 @@ int main()
 				{
 					if (enemies[i].getGlobalBounds().intersects(projectiles[j].BulletGlobalBounds()))
 					{
-						hitcount+=bullet_strength;
+						hitcount += bullet_strength;
 						if (enemies[i].getColor() == sf::Color::Yellow)
 						{
 							hit = 2;
@@ -467,7 +524,6 @@ int main()
 						{
 							killCount += kill;
 							hitcount = 0;
-
 							if (enemies[i].getColor() == sf::Color::Cyan)
 							{
 								dead1++;
@@ -484,7 +540,23 @@ int main()
 							{
 								enemies.erase(enemies.begin() + i);
 							}
-							
+						}
+						if (hitcount >= hit)
+						{
+							killCount += kill;
+							hitcount = 0;
+							if (enemies[i].getColor() == sf::Color::Cyan)
+							{
+								dead1++;
+								Heart.setPosition(enemies[i].getPosition());
+							}
+							else if (enemies[i].getColor() == sf::Color::Magenta)
+							{
+								dead2++;
+								Amno.setPosition(enemies[i].getPosition());
+							}
+							is_dead[pos] = i;
+							pos++;
 						}
 
 						projectiles.erase(projectiles.begin() + j);
@@ -499,7 +571,7 @@ int main()
 			}
 			for (int i = 0; i < Hearts.size(); i++)
 			{
-				Hearts[i].move(0.0f, 0.9f + float(killCount / 100.f));			
+				Hearts[i].move(0.0f, 0.9f + float(killCount / 100.f));
 				if (player.GetGlobalBounds().intersects(Hearts[i].getGlobalBounds()))
 				{
 					Hearts.erase(Hearts.begin() + i);
@@ -520,20 +592,18 @@ int main()
 					Amnos.erase(Amnos.begin() + i);
 				}
 			}
-			
-		
+
 			//animations
 			animation_player.Update(0, deltaTime);
-			animation_enemy.Update(0, deltaTime);
 			animation_meteors.Update(0, deltaTime);
 			animaion_hearts.Update(0, deltaTime);
 			animation_amno.Update(0, deltaTime);
+			animation_enemy.Update(0, deltaTime);
 		}
 
 		window.clear(sf::Color::Black);
 		if (Is_Game_Start == 1)
 		{
-
 			for (int i = 0; i < stars.size(); i++)
 			{
 				window.draw(stars[i]);
@@ -546,6 +616,7 @@ int main()
 			player.SetTextureRect(animation_player.uvRect);
 			player.Draw(window);
 			window.draw(Score);
+
 			for (int i = 0; i < Hearts.size(); i++)
 			{
 				Hearts[i].setTextureRect(animaion_hearts.uvRect);
@@ -584,9 +655,37 @@ int main()
 				{
 					stars.erase(stars.begin() + i);
 				}
-				
 
+				// Writing New High Scores
+				HighScoresFile.open("High Scores.txt", ios::out);
+				if (!HighScoresFile)
+				{
+					cout << "Unable to open the High Scores file" << endl;
+				}
+				int something = killCount;
+				for (int i = 0; i < countOfScores; i++)
+				{
+					if (something > HighScoresOfPlayers[i])
+					{
+						int* temp = new int;
+						*temp = HighScoresOfPlayers[i];
+						HighScoresOfPlayers[i] = something;
+						something = *temp;
+						delete temp;
+					}
+					else if (something == HighScoresOfPlayers[i])
+					{
+						break;
+					}
+				}
 
+				for (int i = 0; i < countOfScores; i++)
+				{
+					HighScoresFile << HighScoresOfPlayers[i] << endl;
+				}
+				HighScoresFile.close();
+
+				Count.setString(to_string(killCount));
 				Score.setPosition(window.getSize().x / 2 - 50, window.getSize().y / 2);
 				Count.setPosition(window.getSize().x / 2 - 25, window.getSize().y / 2 + 50);
 				GameTitle.setPosition(MainScreen.getPosition().x + MainScreen.getSize().x / 2.0, MainScreen.getPosition().y + MainScreen.getSize().y / 2.0);
@@ -595,15 +694,15 @@ int main()
 				Quit.setPosition(MainScreen.getSize().x / 2.f, MainScreen.getSize().y / 2 + 200);
 				Restart.setPosition(MainScreen.getSize().x / 2.f, MainScreen.getSize().y / 2 + 150.f);
 
-
 				if (!(Restart.getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(window))) || (Quit.getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(window))))))
 				{
 					Select.play();
 				}
-			
+
 				if (Restart.getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(window))))
 				{
-					Restart.setFillColor(sf::Color::Red);
+					Restart.setFillColor(sf::Color::Blue);
+					Restart.setOutlineColor(sf::Color::White);
 					Restart.setPosition(Restart.getPosition().x, Restart.getPosition().y - 5);
 					if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 					{
@@ -617,7 +716,6 @@ int main()
 						dead1 = -1;
 						dead2 = -1;
 						StarCount = 0;
-						killCount = 0;
 						bullet_strength = 1;
 						Score.setPosition(window.getSize().x - 230, 200);
 						Count.setPosition(window.getSize().x - 180, 250);
@@ -627,11 +725,12 @@ int main()
 						startGame = 0;
 					}
 				}
-				
+
 
 				if (Quit.getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(window))))
 				{
-					Quit.setFillColor(sf::Color::Red);
+					Quit.setFillColor(sf::Color::Blue);
+					Quit.setOutlineColor(sf::Color::White);
 					Quit.setPosition(Quit.getPosition().x, Quit.getPosition().y - 5);
 					if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 					{
@@ -639,37 +738,109 @@ int main()
 					}
 				}
 				window.clear(sf::Color::Black);
-				window.draw(txt);
+				window.draw(GameOver);
 				window.draw(Score);
 				window.draw(Count);
 				window.draw(Quit);
 				window.draw(Restart);
 			}
 		}
+		else if (HighScorePanel == 1)
+		{
+			int SpaceHaiYaarScoreKBachKa = 50;
+
+			sf::Text* Back;
+			Back = new sf::Text;
+			Back->setString("BACK [Enter]");
+			Back->setFont(TitleFont);
+			Back->setCharacterSize(MainScreen.getSize().y / 40.0);
+			Back->setOrigin(Back->getGlobalBounds().width / 2.0, Back->getGlobalBounds().height / 2.0);
+			Back->setPosition(window.getSize().x / 2.0, window.getSize().y * 0.95);
+			Back->setStyle(sf::Text::Bold);
+			Back->setFillColor(sf::Color::White);
+			Back->setOutlineColor(sf::Color::Blue);
+			Back->setOutlineThickness(3);
+
+			HighScoresScreen.setFillColor(sf::Color(255, 255, 0, 0));
+			HighScores.setPosition(window.getSize().x / 2.0, window.getSize().y * 0.15);
+
+			sf::Text* Temp;
+			Temp = new sf::Text;
+			Temp->setFont(TitleFont);
+			Temp->setCharacterSize(MainScreen.getSize().y / 30.0);
+			Temp->setOrigin(Temp->getGlobalBounds().width / 2.0, Temp->getGlobalBounds().height / 2.0);
+			Temp->setStyle(sf::Text::Bold);
+			Temp->setFillColor(sf::Color::White);
+			Temp->setOutlineColor(sf::Color::Blue);
+			Temp->setOutlineThickness(3);
+
+
+			for (int i = 0; i < countOfScores; i++)
+			{
+				Temp->setString(to_string(HighScoresOfPlayers[i]));
+				Temp->setPosition(window.getSize().x / 2.0, HighScores.getPosition().y + SpaceHaiYaarScoreKBachKa);
+				window.draw(*Temp);
+				SpaceHaiYaarScoreKBachKa += 50;
+			}
+
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter))
+			{
+				Back->setFillColor(sf::Color::Blue);
+				Back->setOutlineColor(sf::Color::White);
+				HighScorePanel = 0;
+				delete Back;
+				delete Temp;
+			}
+			// It was creating problem if i delete it then tried to draw it.
+			if (HighScorePanel == 1)
+			{
+				window.draw(*Back);
+			}
+
+			window.draw(HighScores);
+			window.draw(HighScoresScreen);
+		}
 		else
 		{
+			//Intro.play();
 			GameTitle.setPosition(MainScreen.getPosition().x + MainScreen.getSize().x / 2.0, MainScreen.getPosition().y + MainScreen.getSize().y / 2.0);
 			Picture0.setPosition(MainScreen.getPosition().x + MainScreen.getSize().x / 2, MainScreen.getPosition().y + MainScreen.getSize().y / 2);
 			Start.setPosition(MainScreen.getPosition().x + MainScreen.getSize().x / 2.0, MainScreen.getPosition().y + MainScreen.getSize().y / 2.0);
-			Quit.setPosition(MainScreen.getPosition().x + MainScreen.getSize().x / 2.0, MainScreen.getPosition().y + MainScreen.getSize().y / 2.0 + GameTitle.getGlobalBounds().height + Start.getGlobalBounds().height + Quit.getGlobalBounds().height);
+			HighScores.setPosition(MainScreen.getPosition().x + MainScreen.getSize().x / 2.0, MainScreen.getPosition().y + MainScreen.getSize().y / 2.0 + GameTitle.getGlobalBounds().height + Start.getGlobalBounds().height + HighScores.getGlobalBounds().height);
+			Quit.setPosition(MainScreen.getPosition().x + MainScreen.getSize().x / 2.0, MainScreen.getPosition().y + MainScreen.getSize().y / 2.0 + GameTitle.getGlobalBounds().height + Start.getGlobalBounds().height + HighScores.getGlobalBounds().height + Quit.getGlobalBounds().height + 30);
 
-			if (!(Start.getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(window))) || Quit.getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(window)))))
+			if (!(Start.getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(window))) || Quit.getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(window))) || HighScores.getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(window)))))
 			{
 				Select.play();
 			}
 
 			if (Start.getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(window))))
 			{
-				Start.setFillColor(sf::Color::Red);
+				
+				Start.setFillColor(sf::Color::Blue);
+				Start.setOutlineColor(sf::Color::White);
 				Start.setPosition(Start.getPosition().x, Start.getPosition().y - 5);
 				if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 				{
 					startGame = 1;
 				}
 			}
+			else if (HighScores.getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(window))))
+			{
+			
+				HighScores.setFillColor(sf::Color::Blue);
+				HighScores.setOutlineColor(sf::Color::White);
+				HighScores.setPosition(HighScores.getPosition().x, HighScores.getPosition().y - 5);
+				if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+				{
+					HighScorePanel = 1;
+				}
+			}
 			else if (Quit.getGlobalBounds().contains(sf::Vector2f(sf::Mouse::getPosition(window))))
 			{
-				Quit.setFillColor(sf::Color::Red);
+		
+				Quit.setFillColor(sf::Color::Blue);
+				Quit.setOutlineColor(sf::Color::White);
 				Quit.setPosition(Quit.getPosition().x, Quit.getPosition().y - 5);
 				if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 				{
@@ -692,6 +863,7 @@ int main()
 			window.draw(Picture0);
 			window.draw(GameTitle);
 			window.draw(Start);
+			window.draw(HighScores);
 			window.draw(Quit);
 		}
 
